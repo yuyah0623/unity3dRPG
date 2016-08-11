f5cac07f8d71a93733935b7386657967aa7c7fd8
平田裕哉 <yuya.h0623@gmail.com>
2016-08-12 01:32:39 +0900

    Treasure chest open if player hit.

--- Assets/Script/TreasureChest.cs
+++ Assets/Script/TreasureChest.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class TreasureChest : MonoBehaviour {
 
+	Animator anim;
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.name == "Player") {
+			anim.SetBool ("IsOpen", true);
+		}
+	}
+
 }
